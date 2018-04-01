535e4797483952c29c810acc35cd82e7d0f73e7e
alecgavrilovich <alecgavrilovcih@gmail.com>
2018-04-01 17:48:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple webpack starter for Vanilla JS projects
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'index', template: './src/index.html' })
+  ]
+};
